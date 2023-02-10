9699900a6da8aeb05790fb646b05a7e07c011b47
kittenmasque <volaju@gmail.com>
2023-02-09 19:36:10 -0800

    Increments count every time button is clicked via implementing ActionListener

--- src/GUI.java
+++ src/GUI.java
@@ -1,17 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    int count = 0;
+    JLabel label;
 
     public GUI() {
         JFrame frame = new JFrame();
 
         JButton button = new JButton("Click me");
+        button.addActionListener(this);
+
+        label = new JLabel("Number of clicks: 0");
 
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
         panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,4 +33,9 @@ public class GUI {
         new GUI();
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
 }

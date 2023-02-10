8fc835b600ffe954e597373f2abb143ccd4b5f91
kittenmasque <volaju@gmail.com>
2023-02-09 19:23:37 -0800

    Creates a small GUI window with clickable button and default operation to exit on close

--- src/GUI.java
+++ src/GUI.java
@@ -5,9 +5,13 @@ public class GUI {
 
     public GUI() {
         JFrame frame = new JFrame();
+
+        JButton button = new JButton("Click me");
+
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
